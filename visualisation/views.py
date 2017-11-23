6d448397d0cdcb822f07421f8c61e1fd616e33f3
2081252C <carly.crawford317@gmail.com>
2017-11-23 15:16:18 +0000

    graph working with multiple y axes and two sets of data

--- visualisation/views.py
+++ visualisation/views.py
@@ -3,6 +3,7 @@ from registration.models import UserProfile
 from django.contrib.auth.models import User
 from bokeh.plotting import figure, output_file, show 
 from bokeh.embed import components
+from bokeh.models import LinearAxis, Range1d
 
 # Create your views here.
 def index(request):
@@ -15,17 +16,27 @@ def client1(request):
         if request.method == 'GET' and request.user.is_authenticated():
             user_profile = UserProfile.objects.get(user=request.user)
             is_supervisor = user_profile.supervisor
-        x= [1,3,5,7,9,11,13]
-        y= [1,2,3,4,5,6,7]
+        #x= [1,2,3,4,5,6,7]
+	x=["20-Oct-17", "27-Oct-17", "02-Nov-17", "09-Nov-17", "16-Nov-17", "23-Nov-17", "30-Nov-17"]
+        y= [2500,2250,2300, 2150,2050,2100,2200]
         title = 'Weight and Calories over Time'
 
-        plot = figure(title= title , 
+        plot = figure(title= title ,
+	    x_range = x, 
+	    y_range = Range1d(2000, 2500),
             x_axis_label= 'Time', 
             y_axis_label= 'Calories', 
-            plot_width =400,
+            plot_width =600,
             plot_height =400)
 
-        plot.line(x, y, legend= 'f(x)', line_width = 2)
+        plot.line(x, y, legend= 'calories over time', line_width = 2)
+
+	extra_y = [85, 84, 82, 83, 83, 82, 85]
+	plot.extra_y_ranges['weight'] = Range1d(70, 90)
+
+	#plot.multi_line([x, y], [x, extra_y], color=["firebrick", "navy"], line_width=4)
+        plot.add_layout(LinearAxis(y_range_name="weight", axis_label="Weight"), 'right') 
+	plot.line(x, extra_y, legend= 'weight over time', line_width = 2, y_range_name="weight", color="firebrick")
         #Store components 
         script, div = components(plot)
 
@@ -63,8 +74,8 @@ def client3(request):
         if request.method == 'GET' and request.user.is_authenticated():
             user_profile = UserProfile.objects.get(user=request.user)
             is_supervisor = user_profile.supervisor
-        x= [1,3,5,7,9,11,13]
-        y= [1,2,3,4,5,6,7]
+        x= [10,3,5,7,9,11,13]
+        y= [1,2,3,4,5,6,70]
         title = 'y = f(x)'
 
         plot = figure(title= title , 
